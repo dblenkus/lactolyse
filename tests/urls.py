d2085c5ca61eb4c38aa8b313ba2706236cfa0249
Domen Blenkuš <domen@blenkus.com>
2018-06-09 19:07:10 +0200

    Rename test project and move related files into it

--- tests/urls.py
+++ tests/urls.py
@@ -1,4 +1,4 @@
-"""pulse URL Configuration
+"""Test URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
