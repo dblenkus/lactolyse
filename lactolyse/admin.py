9c43d0adb5eea5753857ea0cff994ab508bd5157
Domen Blenkuš <domen@blenkus.com>
2018-06-12 15:46:24 +0200

    Add admin view for lactate threshold analysis

--- lactolyse/admin.py
+++ lactolyse/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from lactolyse.models import LactateThresholdAnalyses
+
+
+class LactateThresholdAnalysesAdmin(admin.ModelAdmin):
+    """Initial group configuration."""
+
+    model = LactateThresholdAnalyses
+
+    list_display = (
+        'get_athlete',
+        'created',
+        'result_dmax',
+        'result_cross',
+        'result_at4',
+        'result_at2',
+    )
+
+    def get_athlete(self, obj):
+        return obj.athlete.name
+    get_athlete.short_description = 'Athlete'
+    get_athlete.admin_order_field = 'athlete__name'
+
+
+admin.site.register(LactateThresholdAnalyses, LactateThresholdAnalysesAdmin)
