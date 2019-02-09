758ef0c88000274410bc8f6cf8628b8c2129b35f
Domen Blenkuš <domen@blenkus.com>
2019-02-09 23:34:26 +0100

    Simplify file reading in download view

--- src/lactolyse/views/download.py
+++ src/lactolyse/views/download.py
@@ -1,7 +1,6 @@
 """Lactolyse download views."""
 import os
 
-from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views import View
 
@@ -19,11 +18,7 @@ class ReportDownloadView(View):
         if analysis.contributor != request.user and not request.user.is_superuser:
             return HttpResponseBadRequest()
 
-        file_path = os.path.join(settings.MEDIA_ROOT, analysis.report.name)
-
-        with open(file_path, 'rb') as fn:
-            response = HttpResponse(fn.read())
-
+        response = HttpResponse(analysis.report.read())
         response['Content-Type'] = 'application/pdf'
         response['Content-Length'] = os.path.getsize(file_path)
         response['Content-Disposition'] = 'attachment; filename="Report.pdf"'
